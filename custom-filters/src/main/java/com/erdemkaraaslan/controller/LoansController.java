a680df018867eb60884e8c759a45e1f2e68c5bee
Erdem Karaaslan <erdem.karaaslan@volantx.com>
2024-09-30 05:29:23 +0300

    Custom filters in spring security
    Security filter chain yapısına 4 adet yeni filtre eklenmiştir.

---
+++ custom-filters/src/main/java/com/erdemkaraaslan/controller/LoansController.java
@@ -0,0 +1,28 @@
+package com.erdemkaraaslan.controller;
+
+import com.erdemkaraaslan.model.Loans;
+import com.erdemkaraaslan.repository.LoanRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class LoansController {
+
+    private final LoanRepository loanRepository;
+
+    @GetMapping("/myLoans")
+    public List<Loans> getLoanDetails(@RequestParam long id) {
+        List<Loans> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(id);
+        if (loans != null) {
+            return loans;
+        } else {
+            return null;
+        }
+    }
+
+}
